fed3a229dab40d2c1044d12da94aa598a4eb8ae1
Olders <Olders@192.168.1.156>
2018-05-08 17:11:56 +0200

    TRas clase reserva

--- HotelAltair/src/main/java/es/altair/hotelAltair/bean/Reserva.java
+++ HotelAltair/src/main/java/es/altair/hotelAltair/bean/Reserva.java
@@ -1,5 +1,143 @@
 package es.altair.hotelAltair.bean;
 
-public class Reserva {
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="reserva")
+public class Reserva implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idReserva;
+	
+	@ManyToOne(cascade=CascadeType.MERGE)
+	@JoinColumn(name="idCliente")
+	private Cliente cliente;
+	
+	@ManyToOne(cascade=CascadeType.MERGE)
+	@JoinColumn(name="idTrabajador")
+	private Trabajador trabajador;
+	
+	@ManyToOne(cascade=CascadeType.MERGE)
+	@JoinColumn(name="idHabitacion")
+	private Habitacion habitacion;
+	
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	String fechaEntrada;
+	
+	String fechaSalida;
+	
+	double precioActual;
+	double precioAPagar;
+	
+	public Reserva() {
+		
+	}
+	
+	public Reserva(Cliente cliente, Trabajador trabajador, Habitacion habitacion, Date fechaEntrada, Date fechaSalida, double precioActual, double precioAPagar) {
+	
+		this.cliente = cliente;
+		this.trabajador = trabajador;
+		this.habitacion = habitacion;
+		this.fechaEntrada = sdf.format(fechaEntrada);
+		this.fechaSalida = sdf.format(fechaSalida);
+		this.precioActual = precioActual;
+		this.precioAPagar = precioAPagar;
+		
+	}
+
+	public int getIdReserva() {
+		return idReserva;
+	}
+
+	public void setIdReserva(int idReserva) {
+		this.idReserva = idReserva;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Trabajador getTrabajador() {
+		return trabajador;
+	}
+
+	public void setTrabajador(Trabajador trabajador) {
+		this.trabajador = trabajador;
+	}
+
+	public Habitacion getHabitacion() {
+		return habitacion;
+	}
+
+	public void setHabitacion(Habitacion habitacion) {
+		this.habitacion = habitacion;
+	}
+
+	public SimpleDateFormat getSdf() {
+		return sdf;
+	}
+
+	public void setSdf(SimpleDateFormat sdf) {
+		this.sdf = sdf;
+	}
+
+	public String getFechaEntrada() {
+		return fechaEntrada;
+	}
+
+	public void setFechaEntrada(String fechaEntrada) {
+		this.fechaEntrada = fechaEntrada;
+	}
+
+	public String getFechaSalida() {
+		return fechaSalida;
+	}
+
+	public void setFechaSalida(String fechaSalida) {
+		this.fechaSalida = fechaSalida;
+	}
+
+	public double getPrecioActual() {
+		return precioActual;
+	}
+
+	public void setPrecioActual(double precioActual) {
+		this.precioActual = precioActual;
+	}
+
+	public double getPrecioAPagar() {
+		return precioAPagar;
+	}
+
+	public void setPrecioAPagar(double precioAPagar) {
+		this.precioAPagar = precioAPagar;
+	}
+
+	@Override
+	public String toString() {
+		return "Reserva [idReserva=" + idReserva + ", cliente=" + cliente + ", trabajador=" + trabajador
+				+ ", habitacion=" + habitacion + ", sdf=" + sdf + ", fechaEntrada=" + fechaEntrada + ", fechaSalida="
+				+ fechaSalida + ", precioActual=" + precioActual + ", precioAPagar=" + precioAPagar + "]";
+	}
+	
+	
+	
+	
 }
--- HotelAltair/src/main/java/es/altair/hotelAltair/bean/Trabajador.java
+++ HotelAltair/src/main/java/es/altair/hotelAltair/bean/Trabajador.java
@@ -1,5 +1,127 @@
 package es.altair.hotelAltair.bean;
 
-public class Trabajador {
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="trabajador")
+public class Trabajador implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idTrabajador;
+	private String nombre;
+	private String apellidos;
+	private int tipoAcceso;
+	private int edad;
+	private String idioma;
+	private String correo;
+	private String password;
+	
+	@OneToMany(mappedBy="trabajador", cascade=CascadeType.ALL)
+	public Set<Reserva> reservas = new HashSet<Reserva>();
+	
+	public Trabajador() {
+		
+	}
+	
+	public Trabajador(String nombre, String apellidos, int tipoAcceso, int edad, String idioma, String correo, String password) {
+		
+		this.nombre= nombre;
+		this.apellidos= apellidos;
+		this.tipoAcceso= tipoAcceso;
+		this.edad= edad;
+		this.idioma= idioma;
+		this.correo= correo;
+		this.password= password;
+	}
+
+	public int getIdTrabajador() {
+		return idTrabajador;
+	}
+
+	public void setIdTrabajador(int idTrabajador) {
+		this.idTrabajador = idTrabajador;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public int getTipoAcceso() {
+		return tipoAcceso;
+	}
+
+	public void setTipoAcceso(int tipoAcceso) {
+		this.tipoAcceso = tipoAcceso;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getIdioma() {
+		return idioma;
+	}
+
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Reserva> getReservas() {
+		return reservas;
+	}
+
+	public void setReservas(Set<Reserva> reservas) {
+		this.reservas = reservas;
+	}
+
+	@Override
+	public String toString() {
+		return "Trabajador [idTrabajador=" + idTrabajador + ", nombre=" + nombre + ", apellidos=" + apellidos
+				+ ", tipoAcceso=" + tipoAcceso + ", edad=" + edad + ", idioma=" + idioma + ", correo=" + correo
+				+ ", password=" + password + "]";
+	}
+	
 }
